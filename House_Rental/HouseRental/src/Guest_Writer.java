78010edd602286bb96a7e8fd3485379e69bad05f
Ma77C0nd0n <matt.condon@ucdconnect.ie>
2015-11-07 15:25:56 +0000

    I'm scared

--- House_Rental/HouseRental/src/Guest_Writer.java
+++ House_Rental/HouseRental/src/Guest_Writer.java
@@ -1,16 +1,14 @@
 import java.io.*;
-import java.nio.file.Path;
 public class Guest_Writer {
+	private String dataFile;
+	private String text = "\n101\t105\t3\t\"House is real\"";
+	private Writer output;
 	Rating rating;
-	File file = new File("../HouseRental/src/u.user");
-	public Guest_Writer(Rating r) {
-		rating = r;
-		if(file.exists()) {
-			System.out.println("YAY");
-		}
-		else{
-			System.out.println("DAM TIS SUKKS");
-		}
+	File file = new File("src/u.user");
+	public Guest_Writer(Rating r) throws IOException {
+		output = new BufferedWriter(new FileWriter("src/u.data", true));
+		output.append(text);
+		output.close();
 	}
 	private void appendFile() {
 		
