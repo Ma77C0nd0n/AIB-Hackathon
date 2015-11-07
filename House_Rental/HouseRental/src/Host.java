3db4f3cedc3aa230ceb28b907bed84fe710d91f1
Ma77C0nd0n <matt.condon@ucdconnect.ie>
2015-11-07 11:47:26 +0000

    Host default methods
    Not yet implemented.
    Now adding datasets (maybe 5 users)

--- House_Rental/HouseRental/src/Host.java
+++ House_Rental/HouseRental/src/Host.java
@@ -3,13 +3,30 @@ public class Host extends User {
 
 	public Host(Integer uid) {
 		super(uid);
-		
 		//address, price, available, userHostRating
 		}
 		
-	private void Address(){
-		
-		
+	private String Address(){
+		return default_address;
+	}
+	
+	private double Price(){
+		return default_price;
+	}
+	
+	private boolean Availability(){
+		return availability;
+	}
+	
+	private double UserHostRating(){
+		return -1;	//-1 implies no rating is available for this user
 	}
 
+	
+	private static double MIN_RATING = 1;
+	private static double MAX_RATING = 5;
+	private double default_price = 0;
+	private String default_address = "My House";
+	private boolean availability = false;
+	
 }
